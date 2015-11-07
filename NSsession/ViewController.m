fd385231d50bbd02ca3ff543b6915cf2801e7d4e
Jaykong <kongyunpeng2011@sina.com>
2015-11-08 02:01:53 +0800

    Revert "Merge branch 'master' into session-branch"
    This reverts commit 34218505ccfb078d9f5ebba31f6bc6e29e94cf4d, reversing
    changes made to 67bacf6ec590d266cbeeafc03c92e82f1ad64e40.

--- NSsession/ViewController.m
+++ NSsession/ViewController.m
@@ -16,6 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSURL *url = [NSURL URLWithString:@"http://112.124.122.38/abc.php"];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+  NSURLSessionDataTask *_tast =  [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"completed");
+      NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+      NSLog(@"%@",output);
+    }];
+    [_tast resume];
+                  
     // Do any additional setup after loading the view, typically from a nib.
 }
 
